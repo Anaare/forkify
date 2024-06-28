f8af5e43c2d9d8e8f9b981464b19a16e9f91e018
Anaare <arevadzeana10@gmail.com>
2024-06-28 09:56:43 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'f24eda92-2cb7-4e5b-bbc3-3a814695bea2';
+export const MODAL_CLOSE_SEC = 2.5;
